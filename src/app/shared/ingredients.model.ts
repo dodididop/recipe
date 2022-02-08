87376a4f6b8ca6f6701f6a0a0bbb5c2be70cb25c
Digdem Cokyasar <dcokyasar@promaks.com.tr>
2022-02-08 15:36:40 +0300

    The basics of Angular | First Commit

---
+++ src/app/shared/ingredients.model.ts
@@ -0,0 +1,11 @@
+export class Ingredient{
+    // public name: string
+    // public amount: number
+    // constructor(name: string, amount: number){
+    //     this.name = name
+    //     this.amount = amount
+    // } 
+    // below code is shortcut of above commented code.
+    constructor(public name:string, public amount:number){}
+    
+}
